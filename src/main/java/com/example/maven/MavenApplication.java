d18fc20d04d655f57cf0319e0b4747712956e275
Chrisjohn <chrisjohnd@Chrisjohns-MacBook-Pro.local>
2023-05-22 01:44:58 -0700

    adding first project maven

---
+++ src/main/java/com/example/maven/MavenApplication.java
@@ -0,0 +1,30 @@
+package com.example.maven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+@EnableAutoConfiguration
+@SpringBootApplication
+public class MavenApplication {
+
+	@RequestMapping("/")
+	@ResponseBody
+	String home(){
+		return "Hello World!";
+	}
+	@RequestMapping("about")
+	@ResponseBody
+	String about(){
+		return "Sample Maven Build";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenApplication.class, args);
+		
+	}
+
+}
